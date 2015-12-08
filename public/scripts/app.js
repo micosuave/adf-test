4f7f81bc4ec03490f2d4b708a7de748b2b4f0ec3
Michael Costa <michaelmcosta@me.com>
2015-12-08 06:27:02 -0500

    ad

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -29,19 +29,31 @@ angular.module('adfDynamicSample', [
     'adf.widget.clock', 'adf.widget.github', 'adf.widget.iframe',
     'adf.widget.linklist', 'adf.widget.markdown', 'adf.widget.news',
     'adf.widget.randommsg', 'adf.widget.version', 'adf.widget.weather',
-    'adf.widget.testwidget','adf.widget.treewidget'
+    'adf.widget.testwidget','adf.widget.treewidget','adf.widget.getphd','adf.widget.collectionwidget','adf.widget.pagebuilderwidget','adf.widget.wysiwyg','auth0'
   ])
   .config(function($stateProvider){
     $stateProvider
       
-      .state('boards', {
+      .state('home', {
         url: '',
         templateUrl: 'partials/default.html'
       })
+      .state('boards',{
+        parent: 'home',
+        url: '/boards',
+        views: {'@home': {controller: 'dashboardCtrl',
+        controllerAs: 'dashboard',
+        templateUrl: 'partials/dashboard.html'}},
+        resolve: {
+          data: function($stateParams, storeService, $rootScope){
+            return storeService.get($rootScope.authData.id);
+          }
+        }
+      })
       .state('board', {
         parent: 'boards',
-        url: '/boards/:id',
-        views: {'': {controller: 'dashboardCtrl',
+        url: '/:id',
+        views: {'@home': {controller: 'dashboardCtrl',
         controllerAs: 'dashboard',
         templateUrl: 'partials/dashboard.html'}},
         resolve: {
@@ -112,7 +124,7 @@ angular.module('adfDynamicSample', [
 		}
 	]
 )
-  .controller('navigationCtrl', function($scope, $q, $location, storeService, $state){
+  .controller('navigationCtrl', function($scope, $q, $location, storeService, $state, $rootScope){
     var nav = this;
     nav.navCollapsed = true;
 
@@ -126,7 +138,7 @@ angular.module('adfDynamicSample', [
     };
 
     this.create = function(){
-      var id = '_' + new Date().getTime();
+      var id = $rootScope.authData.id + '_' + new Date().getTime();
       var q = storeService.set(id, {
         "title": "New Page",
         "structure": "4-8",
@@ -170,4 +182,131 @@ angular.module('adfDynamicSample', [
     $scope.$on('adfDashboardChanged', function(event, name, model) {
       storeService.set(name, model);
     });
-  });
+  }).controller('LoginCtrl', ['$scope', '$http', 'auth', 'store', '$location', '$state', '$firebaseAuth', 'Auth','$rootScope', 'FIREBASE_URL', 'Profile','$config','$document',
+    function ($scope, $http, auth, store, $location, $state, $firebaseAuth, Auth,$rootScope, FIREBASE_URL, Profile, $config, $document) {
+        $scope.login = function () {
+            auth.signin({}, function (profile, token) {
+                // Success callback
+                store.set('profile', profile);
+                $rootScope.authData = profile;
+                $rootScope.authData.uid = profile.global_client_id;
+                //Profile().$save(profile);
+                store.set('token', token);
+                    
+                var amOnline = new Firebase(FIREBASE_URL + '.info/connected');
+            var userRef = new Firebase(FIREBASE_URL + 'presence/' + $rootScope.authData.uid);
+            // since I can connect from multiple devices or browser tabs, we store each connection instance separately
+            // any time that connectionsRef's value is null (i.e. has no children) I am offline
+            var myConnectionsRef = new Firebase(FIREBASE_URL + 'profiles/' + $rootScope.authData.uid + '/connections');
+
+            // stores the timestamp of my last disconnect (the last time I was seen online)
+            var lastOnlineRef = new Firebase(FIREBASE_URL + 'profiles/' + $rootScope.authData.uid + '/lastOnline');
+            var connectedRef = new Firebase(FIREBASE_URL + '.info/connected');
+            var lastLoginRef = new Firebase(FIREBASE_URL + 'profiles/' + $rootScope.authData.uid + '/lastLogin');
+
+            var connectedRef = new Firebase(FIREBASE_URL + '.info/connected');
+            connectedRef.on('value', function (snap) {
+                if (snap.val() === true) {
+                    // We're connected (or reconnected)! Do anything here that should happen only if online (or on reconnect)
+
+                    // add this device to my connections list
+                    // this value could contain info about the device or a timestamp too
+                    var con = myConnectionsRef.push(true);
+                    lastLoginRef.set(Firebase.ServerValue.TIMESTAMP);
+
+                    // when I disconnect, remove this device
+                    con.onDisconnect().remove();
+
+                    // when I disconnect, update the last time I was seen online
+                    lastOnlineRef.onDisconnect().set(Firebase.ServerValue.TIMESTAMP);
+                }
+                $config().$bindTo($scope, 'config');
+                Cursor().$bindTo($scope, 'cursor');
+                angular.element('body').on('mousedown', function (event) {
+                    $scope.cursor = {
+                        posX: event.clientX,
+                        posY: event.clientY
+                    };
+                    $ACTIVEROAR.scope = event;
+                }).on('keydown', function (event) {
+                    var x = event.which || event.keyCode;
+                    if (x === 20) {
+                        $rootScope.$broadcast('CAPSLOCK');
+                        alertify.warning('CAPSLOCK PRESSED');
+                    } else if (event.altKey === true && x !== 79) {
+                        event.preventDefault();
+                        $rootScope.$broadcast('ALTKEY DOWN');
+                        //alertify.log("¡" + x);
+                        console.log(event);
+                    } else if (event.altKey === true && x === 79) {
+                        $rootScope.$broadcast('OPTIONS');
+                        alertify.success('OPTIONS');
+                    } else if (event.altKey === true && x === 191) {
+                        $rootScope.$broadcast('OPTION?');
+                        alertify.success(x);
+                    }
+                    /* Act on the event */
+                }).on('keyup', function (event) {
+                    if (event.altKey === true) {
+                        $rootScope.$broadcast('ALTKEY UP');
+                        //toastr.success('ALTKEY UP');
+                    }
+                });
+                // var image = angular.element('<img src="img/GoldLion.svg" width="3.5rem" height="3.5rem" style="position:absolute;left: {{cursor.posX + 20}}px; top: {{cursor.posY + 20}}px;z-index:100000;opacity:0.5;transition: 0.1s all ease-in-out;"/>');
+                // var pointer = $compile(image)($scope);
+                // angular.element('body').append(pointer);
+
+
+
+
+
+
+            });
+            amOnline.on('value', function (snapshot) {
+                if (snapshot.val()) {
+                    userRef.onDisconnect().remove();
+                    userRef.set('★ online');
+                    $document.onIdle = function () {
+                        userRef.set('☆ idle');
+                    };
+                    $document.onAway = function () {
+                        userRef.set('☄ away');
+                    };
+                    $document.onBack = function (isIdle, isAway) {
+                        userRef.set('★ online');
+                    };
+
+                }
+            });
+
+
+                $state.go('home', { groupId: $rootScope.authData.uid });
+                // $scope.authObj.$authWithCustomToken(token, function (err, authData) {
+                //      if (err) {
+                //          alertify.alert(err);
+                // } else {
+                //     $rootScope.authData = authData;
+                //     $state.go('home', { groupId: authData.uid });
+                // }
+                // //$location.path('/');
+                // });
+               
+            }, function () {
+                // Error callback
+            });
+        };
+        $scope.auth = Auth;
+
+        var ref = new Firebase(FIREBASE_URL);
+
+        $scope.authObj = $firebaseAuth(ref);
+
+        var authData = $scope.authObj.$getAuth();
+        
+
+
+            
+        
+    
+
+}]);
